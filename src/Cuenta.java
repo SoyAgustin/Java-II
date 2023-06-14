d25ef282d54daadbc6cbde37f1de06163a7396d2
Agustin <agustinphysx@gmail.com>
2023-06-14 17:39:38 -0600

    codigo cuenta limpio sin comentarios

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,21 +1,11 @@
 
 class Cuenta {
-	private double saldo; //con private no se puede modificar directamente fuera de la clase (este archivo) a esto se le llama encapsulamiento.
+	private double saldo; 
 	private int agencia;
 	private int numero;
-	private Cliente titular = new Cliente(); //es un tipo cliente, ya contiene los atributos de un cliente
+	private Cliente titular = new Cliente(); 
 	
-	//métodos: toda secuencia de pasos para realizar alguna accion
-	
-	//Los métodos que no retornan valor inician con void
-	//Los métodos que retornan valor 
-	
-	//public al inicio del método tiene una funcionalidad que se verá después, para nosotros funciona bien sin "public"
-	
-	//El codigo siguiente es un constructor, se pueden inicializar atributos desde que se crean en el código fuera de aqui
-	
-	
-	private static int total=0; //no pertenece a la instancia, sino a la clse
+	private static int total=0; 
 	public Cuenta(int agencia) {
 		if(agencia <= 0) {
 			System.out.println("No se permite 0");
@@ -28,14 +18,12 @@ class Cuenta {
 	
 	public void depositar(double saldo){
 		this.saldo =  this.saldo + saldo;
-		
-		//this sirve para hacer referencia al atributo y diferenciarlo del parámetro del método.
 	}
 	
 	public boolean retirar(double valor) {
 		if (this.saldo>=valor) {
 			this.saldo = this.saldo-valor;
-			return true; //Es el resultado de la operación, pero no lo imprime, a menos que lo indiquemos, se puede trabajar con este valor haciendo referencia con una nueva variable.
+			return true; 
 		}else {
 			return false;
 		}
@@ -51,7 +39,7 @@ class Cuenta {
 		}
 	}
 	
-	public double getSaldo() { //verbo onbtener en ingles por convención
+	public double getSaldo() { 
 		return this.saldo;
 	}
 	
